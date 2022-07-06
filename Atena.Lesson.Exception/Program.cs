3baf88da4af6b91bcffe33651ac989fff26268a1
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-07-06 12:10:47 +0200

    Add project files.

---
+++ Atena.Lesson.Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using Exception.Library;
+using System;
+
+namespace Atena.Lesson.Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankWebApp ING = new BankWebApp();
+           
+
+            try
+            {
+                ING.Withdraw(10);// --  > esce dal programma
+            }
+            catch (System.Exception ex )
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine(ex.StackTrace);
+                Console.ResetColor();         
+            }
+        } 
+    }
+
+    
+}
---
+++ Exception.Library/ExceptionUtility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception.Library
+{
+    internal class ExceptionUtility
+    {
+       
+    }
+    internal class Bank
+    {
+        internal uint _totalBankMoney = 0 ;  
+        internal void Deposit()
+        {
+
+        }
+        internal void Withdraw(uint amount)
+        {
+           
+                //// aprire un connession al DB 
+                _totalBankMoney -= amount / 0; // ->> ERROR 
+                ///  Chiudere la connessione al DB              
+            
+            WriteOprationOnDB($"Operazione di prelievo completata. {amount} Nome Utente {DateTime.Now}");
+        }
+            
+        internal void WriteOprationOnDB(string msg)
+        {
+            Console.WriteLine(msg); // --> sCRIVO RISULTATO NEL DATABASE
+            Console.WriteLine(msg); // -->  sCRIVO NEL FILE DI LOG
+        }
+    }   
+    public class BankWebApp
+    {
+        Bank Bank  = new Bank();
+        public void Deposit()
+        {
+            Bank.Deposit();
+        }
+        public void Withdraw(uint amount)
+        {
+            // Break -> Throw Exception 
+            try
+            {
+                Bank.Withdraw(amount);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message); //Log
+
+                throw; // -> Finishi la funzione e rimamandaindiretro  l'errrore
+            }
+
+
+        }
+    }
+}
